a411d7b4ee64e87aa216b89c515b10b02be22c52
VictorHugoDC1999 <victorhugodelciampo@gmail.com>
2024-07-26 15:58:33 -0300

    :truck: chore: moving images

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,20 +1,31 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text } from 'react-native';
 import { ProvedorUsuario } from './data/contexts/ContextoUsuario';
 import { ProvedorAgendamento } from './data/contexts/ContextoAgendamento';
+import { NavigationContainer } from '@react-navigation/native';
+import Cadastro from './screens/Cadastro';
+import Principal from './screens/Principal';
+import Sumario from './screens/Sumario';
 
 const Stack = createNativeStackNavigator();
 
-export const App = () => {
+export default function App() {
   return (
     <ProvedorUsuario>
       <ProvedorAgendamento>
-        <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
-          <Text>Mobileaa</Text>
-        </View>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Cadastro"
+              component={Cadastro}
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen name="Principal" component={Principal} />
+            <Stack.Screen name="Sumario" component={Sumario} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </ProvedorAgendamento>
     </ProvedorUsuario>
   );
-};
+}
